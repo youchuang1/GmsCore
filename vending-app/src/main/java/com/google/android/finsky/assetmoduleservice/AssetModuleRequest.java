3718818db7de0722d921d4986f3fea9e8645a52b
Nuyoah <2403930241@qq.com>
2024-08-07 20:21:09 +0800

    Changes

--- vending-app/src/main/java/com/google/android/finsky/assetmoduleservice/AssetModuleRequest.java
+++ vending-app/src/main/java/com/google/android/finsky/assetmoduleservice/AssetModuleRequest.java
@@ -1,6 +1,33 @@
 package com.google.android.finsky.assetmoduleservice;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AuthenticatorException;
+import android.accounts.OperationCanceledException;
+import android.content.Context;
+import android.util.Base64;
+
+import com.android.vending.AndroidVersionMeta;
 import com.android.vending.AssetModuleDeliveryRequest;
+import com.android.vending.DeviceMeta;
+import com.android.vending.EncodedTriple;
+import com.android.vending.EncodedTripleWrapper;
+import com.android.vending.IntWrapper;
+import com.android.vending.LicenseRequestHeader;
+import com.android.vending.Locality;
+import com.android.vending.LocalityWrapper;
+import com.android.vending.StringWrapper;
+import com.android.vending.Timestamp;
+import com.android.vending.TimestampContainer;
+import com.android.vending.TimestampContainer1;
+import com.android.vending.TimestampContainer1Wrapper;
+import com.android.vending.TimestampContainer2;
+import com.android.vending.TimestampStringWrapper;
+import com.android.vending.TimestampWrapper;
+import com.android.vending.UnknownByte12;
+import com.android.vending.UserAgent;
+import com.android.vending.Util;
+import com.android.vending.Uuid;
 import com.android.volley.AuthFailureError;
 import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
@@ -8,34 +35,142 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.HttpHeaderParser;
 
+import org.microg.gms.profile.Build;
+
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
+
+import okio.ByteString;
 
 public final class AssetModuleRequest extends Request<byte[]> {
     private final AssetModuleDeliveryRequest requestPayload;
-    private final Map<String, String> headers;
     private final VolleyCallback callback;
+    private static final String tokentype = "oauth2:https://www.googleapis.com/auth/googleplay";
+    private final Account user;
+    private final Context context;
+    private static final int BASE64_FLAGS = Base64.URL_SAFE | Base64.NO_WRAP | Base64.NO_PADDING;
+    long ANDROID_ID = 1;
+    private static final String FINSKY_VERSION = "Finsky/37.5.24-29%20%5B0%5D%20%5BPR%5D%20565477504";
 
-    public AssetModuleRequest(String url, AssetModuleDeliveryRequest requestPayload, Map<String, String> headers, VolleyCallback callback) {
+    public AssetModuleRequest(Context context, String url, AssetModuleDeliveryRequest requestPayload, Account user, VolleyCallback callback) {
         super(Method.POST, url, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 callback.onError(error.toString());
             }
         });
+        this.context = context;
         this.requestPayload = requestPayload;
-        this.headers = headers;
+        this.user = user;
         this.callback = callback;
     }
 
     @Override
     public Map<String, String> getHeaders() throws AuthFailureError {
+        Locality locality = new Locality.Builder()
+                .unknown1(1)
+                .unknown2(2)
+                .countryCode("")
+                .region(new TimestampStringWrapper.Builder()
+                        .string("").timestamp(makeTimestamp(System.currentTimeMillis())).build())
+                .country(new TimestampStringWrapper.Builder()
+                        .string("").timestamp(makeTimestamp(System.currentTimeMillis())).build())
+                .unknown3(0)
+                .build();
+        String encodedLocality = new String(
+                Base64.encode(locality.encode(), BASE64_FLAGS)
+        );
+        long millis = System.currentTimeMillis();
+        TimestampContainer.Builder timestamp = new TimestampContainer.Builder()
+                .container2(new TimestampContainer2.Builder()
+                        .wrapper(new TimestampWrapper.Builder().timestamp(makeTimestamp(millis)).build())
+                        .timestamp(makeTimestamp(millis))
+                        .build());
+        millis = System.currentTimeMillis();
+        timestamp
+                .container1Wrapper(new TimestampContainer1Wrapper.Builder()
+                        .androidId(String.valueOf(ANDROID_ID))
+                        .container(new TimestampContainer1.Builder()
+                                .timestamp(millis + "000")
+                                .wrapper(makeTimestamp(millis))
+                                .build())
+                        .build()
+                );
+        String encodedTimestamps = new String(
+                Base64.encode(Util.encodeGzip(timestamp.build().encode()), BASE64_FLAGS)
+        );
+        byte[] header = new LicenseRequestHeader.Builder()
+                .encodedTimestamps(new StringWrapper.Builder().string(encodedTimestamps).build())
+                .triple(
+                        new EncodedTripleWrapper.Builder().triple(
+                                new EncodedTriple.Builder()
+                                        .encoded1("")
+                                        .encoded2("")
+                                        .empty("")
+                                        .build()
+                        ).build()
+                )
+                .locality(new LocalityWrapper.Builder().encodedLocalityProto(encodedLocality).build())
+                .unknown(new IntWrapper.Builder().integer(5).build())
+                .empty("")
+                .deviceMeta(new DeviceMeta.Builder()
+                        .android(
+                                new AndroidVersionMeta.Builder()
+                                        .androidSdk(Build.VERSION.SDK_INT)
+                                        .buildNumber(Build.ID)
+                                        .androidVersion(Build.VERSION.RELEASE)
+                                        .unknown(0)
+                                        .build()
+                        )
+                        .unknown1(new UnknownByte12.Builder().bytes(new ByteString(new byte[]{}
+                        )).build())
+                        .unknown2(1)
+                        .build()
+                )
+                .userAgent(new UserAgent.Builder()
+                        .deviceName(Build.DEVICE)
+                        .deviceHardware(Build.HARDWARE)
+                        .deviceModelName(Build.MODEL)
+                        .finskyVersion(FINSKY_VERSION)
+                        .deviceProductName(Build.MODEL)
+                        .androidId(ANDROID_ID) // must not be 0
+                        .buildFingerprint(Build.FINGERPRINT)
+                        .build()
+                )
+                .uuid(new Uuid.Builder()
+                        .uuid(UUID.randomUUID().toString())
+                        .unknown(2)
+                        .build()
+                )
+                .build().encode();
+        String xPsRh = new String(Base64.encode(Util.encodeGzip(header), BASE64_FLAGS));
+        Map<String, String> headers = new HashMap<>();
+        headers.put("x-ps-rh", xPsRh);
+        headers.put("User-Agent", "Android-Finsky/42.0.20-23 [0] [PR] 654119317 (api=3,versionCode=84202000,sdk=33,device=coral,hardware=coral,product=coral,platformVersionRelease=13,model=Pixel%204%20XL,buildId=TP1A.221005.002.B2,isWideScreen=0,supportedAbis=arm64-v8a;armeabi-v7a;armeabi)");
+        try {
+            headers.put("Authorization", "Bearer " + AccountManager.get(context).getAuthToken(user,tokentype,null,false,null,null).getResult().getString(AccountManager.KEY_AUTHTOKEN));
+        } catch (AuthenticatorException | OperationCanceledException | IOException e) {
+            throw new RuntimeException(e);
+        }
+        headers.put("Accept-Language", "zh-CN");
+        headers.put("Content-Type", "application/x-protobuf");
+        headers.put("Accept-Encoding", "gzip, deflate, br");
+        headers.put("Priority", "u=1, i");
         return headers;
     }
 
     @Override
     public byte[] getBody() throws AuthFailureError {
         return AssetModuleDeliveryRequest.ADAPTER.encode(requestPayload);
+    }
+
+    private static Timestamp makeTimestamp(long millis) {
+        return new Timestamp.Builder()
+                .seconds((millis / 1000))
+                .nanos(Math.floorMod(millis, 1000) * 1000000)
+                .build();
     }
 
     @Override
--- vending-app/src/main/java/com/google/android/finsky/assetmoduleservice/AssetModuleService.java
+++ vending-app/src/main/java/com/google/android/finsky/assetmoduleservice/AssetModuleService.java
@@ -1,6 +1,8 @@
 package com.google.android.finsky.assetmoduleservice;
 
+import android.accounts.Account;
 import android.accounts.AccountManager;
+
 import android.accounts.AuthenticatorException;
 import android.accounts.OperationCanceledException;
 import android.app.Service;
@@ -9,10 +11,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
-import android.util.Base64;
+
 import android.util.Log;
 
-import com.android.vending.licensing.LicenseRequest;
 
 import com.android.vending.AssetModuleDeliveryRequest;
 import com.android.vending.Bbvz;
@@ -21,8 +22,7 @@ import com.android.vending.Bdpo;
 import com.android.vending.Bdpp;
 import com.android.vending.IntermediateIntegrityResponseWrapperExtend;
 
-import com.android.volley.NetworkResponse;
-import com.android.volley.Response;
+
 import com.google.android.play.core.assetpacks.protocol.IAssetModuleService;
 import com.google.android.play.core.assetpacks.protocol.IAssetModuleServiceCallback;
 
@@ -37,7 +37,8 @@ import java.util.Map;
 public class AssetModuleService extends Service {
     private static final String TAG = "AssetModuleService";
     private AssetModuleInfo assetModuleInfo;
-    private Context context;
+    public Context context;
+    private Account user;
 
     @Override
     public void onCreate() {
@@ -45,7 +46,16 @@ public class AssetModuleService extends Service {
         context = this;
         // 初始化 AssetModuleInfo 实例
         assetModuleInfo = new AssetModuleInfo(this, getPackageManager(), new AssetModuleController(), new AuthenticationHandler(), new OwnershipChecker());
+        user = getAccount();
+    }
 
+    private Account getAccount() {
+        AccountManager accountManager = AccountManager.get(this);
+        Account[] accounts = accountManager.getAccountsByType("com.google");
+        if (accounts.length > 0) {
+            return accounts[0];
+        }
+        return null; // 处理没有找到账户的情况
     }
 
     private final IAssetModuleService.Stub service = new IAssetModuleService.Stub() {
@@ -91,34 +101,22 @@ public class AssetModuleService extends Service {
                                 .build()))
                         .isInstantApp(false)
                         .build();
-                Map<String, String> headers = new HashMap<>();
-                headers.put("x-ps-rh", "H4sIAAAAAAAAAGVUO4_j1hW2ZmNgocoYwECwTdZGDBgGOMM3NQukOOSlKEq8fIkUh2oGfIniQ6JGpESRlWE3KQM4XYIUThOkCVzHQIAU-QPpUgRBfoKbDbYKZ8dFgNyLe8-HU3znfB_uueOvR-MvRzO2VuHH9cDhjruodJNbsrXXkcTg3O91ze31Hvd4Ky5Z0RpyKtnkmMFIaus8uuBe7uQkP4ThRUg6ye79eLHQaDczMpbQtrQZeAek5lOmj3NVqzYAs2bnRaviqeQv7L-8GH__YvyfkQR42ViyvrOCkNMq_X6NFX6x78XUVc6-LJc-KMz5GCL1MK-ACZBKAWAXw2JuspcuFy2jVyzUAUibBSCQG1U_OhU0bQreWpbB8A3QogWoEwX0VoZ5a8FcXoBIOYAlktvKUFp9MGsRglRUQFOXIK1bECwTjCfCCVAtO6tSF5i248XKNWFBwoPFxqm1lEUL5GFDYflQtC6IluSJVTUFHiQQ3dVpTXcSe9pwaTdbrteIM52dnEyo6-_fS89A1kPhvGvSUwybrhdNP6aWHvLYyM6ssC-aFpNRqoBx2VeHStmThC367QIwPgBUsN9mO8QNEo3oXjTBEbXJirm0UBz7nRxOeFhMAsJae2Cwg5T7FGYdRrjCgFt5YvkFTI2NCKiEFDWD_Hhqnx5qzdfobsYdUuN8jFNcPNZKKXkskfi2QJ32Yl2jVx-sf_di_JsXg3YfJFVyyXghp9UQoyWCTD7enWhxi9htE5KwmovS1r1g5LJ6Dh3uLRo7mMW9y2IHaL1PLbArLJkb4RK6ioyl-9OpFnFaq3kkRA34VmoYhJRXiqeU25CmDlaq56FXLq22Wg0cPc7hYiCV0XN5eJn4MkQa524_9BcgyDcclihxK7UNO3MgV7E0D9KJgBVZlWpa85GR2qY7FU0rXZsusqcWOV_a1hO3zBhOMXABp-cFozvFRe-BNBz58iO3xvD0E7c1me9TcFRNMvfu_igPOpCfIZUXy-3B9-6awFVnkXJX-_fzQ0TNp8-eqBzO02HO3mMWo2ioAd0wec-eGF5QHQdPDDQvfRY9eULFtF2ngyf4bGP0v57ERezple9RrQcQXr388PjBV6N3V-O3VyBO5YZahWdHlzcHW5MOZnZy_JOUJvo8Oxl31i7T2ekdb2kMYz6cqL3qGP70LBTcoj_3XpCJdYnV-2Z5bjRToDKK0IzCs7DNZyZbWu1lUiRqSNm9wDPl3i2adYpYhEKsiUJpKozeVOug16crrfICZlP4lE3Ha59PlEsg-Y-WINtzm4fZHZkqRVgeGWUVXXyeXvQlnsiPFr-R82W1fKy1hYCFjYY0WaiI7QGQUbWh2YuRIiycY63ofDfv2A3PMItwNnXk844yUZkS6dTL_IlGJerwx9SGLZUZdQhKbetXkk7GOvno6vcb26rnp7Dr15tTLT-4NXbWZhmX6Rq3TbRKre1sOYt252YeCQkNkd_EYvogiVNbS9XUdumifSCZ_EySqxNHOOy3ozfjn7385PraMSm4oWmKJLkbkqRvRPrVFcW8fvf2m8-vPw52RLCPj1UWE01S1s0xIILTT0d_HP1-NP4wqo5Bef0cXo3N7JKUr9nX99qn9DTb10V3y9I31A0tEAz1GU1-xonDQc_ItJ8hz00mLMlxky-each__vpff_rztz98x78R06pKy-T2ff75fkMxt__f7u0dM6EZhntzqpPj7TEpk6BOiCLp6n-MPh__nA4ZkhGEiOCoTUiwCRsQIRskREQnJMVzIXNHxR9d_Xv0k5dv_zp6Nxp_9Ie___ZX3_3yh799-l-Pq5UakAYAAA");
-                headers.put("User-Agent", "Android-Finsky/42.0.20-23 [0] [PR] 654119317 (api=3,versionCode=84202000,sdk=33,device=coral,hardware=coral,product=coral,platformVersionRelease=13,model=Pixel%204%20XL,buildId=TP1A.221005.002.B2,isWideScreen=0,supportedAbis=arm64-v8a;armeabi-v7a;armeabi)");
-                headers.put("Authorization", "Bearer ya29.m.CpYCAXQnlZez7qMnpSt8Ld3sbaekzjEX0N72I8JYkAhuuJ07uNYqhlU5-Mlos2RNIKyiD_ZOiSsbHneNQAQJaEUjTe-frJ11POdpZKNueM4qioT6qjqMDhOoxeYEUgqPlkgRLQuWO6tbReVUj7815Id2HdSM7oy6HOXTYPN4-b176Llxg9o1G_Dk6hfth7pnzPVC9JE1_CpFgjJoRuuy0j-YT-q4ibpuj6vlkrrl1HVjJTNmD1BeW3r7kG4ejBjDnFgL3zkcD1IMS5iak2-k1QgzcT87r5_1zIIl3D_7GLKs23cCw0PMXXivZ4TxTcemKc4yCZ6_hWKTXKQs9wXZSctVWSkFS4k5xctb0nggstC9Eodwn_cvf6ESDwgBEgcKAQQQnp0CGNKAAhog735qQOfo3CH6NDEDWKIkcJ-E045gAdTDPg31EyMu9-EiAggBKithQ2dZS0FkY1NBUkVTRlFIR1gyTWlBaEFZS2x3eE5sbTg1bjNCQ0pIYnJB");
-                headers.put("Accept-Language", "zh-CN");
-                headers.put("Content-Type", "application/x-protobuf");
-                headers.put("Accept-Encoding", "gzip, deflate, br");
-                headers.put("Priority", "u=1, i");
+
 
 
                 Log.d(TAG, String.valueOf(requestPayload));
-                Log.d(TAG, headers.toString());
 
 
-                AssetModuleRequest request = new AssetModuleRequest("https://play-fe.googleapis.com/fdfe/assetModuleDelivery",
-                        requestPayload, headers, new AssetModuleRequest.VolleyCallback() {
+                AssetModuleRequest request = new AssetModuleRequest(context,"https://play-fe.googleapis.com/fdfe/assetModuleDelivery",
+                        requestPayload,user, new AssetModuleRequest.VolleyCallback() {
                     @Override
                     public void onSuccess(byte[] result) {
-                        //                            IntermediateIntegrityResponseWrapperExtend response = IntermediateIntegrityResponseWrapperExtend.ADAPTER.decode(result);
-                        Log.d("Response", Arrays.toString(result));
-
-
-                        StringBuilder hexString = new StringBuilder();
-                        for (byte b : result) {
-                            // 使用位运算和字符串格式化将每个字节转换为两位十六进制数
-                            hexString.append(String.format("%02X", b));
+                        try {
+                            IntermediateIntegrityResponseWrapperExtend response = IntermediateIntegrityResponseWrapperExtend.ADAPTER.decode(result);
+                            Log.d("Response", response.toString());
+                        } catch (IOException e) {
+                            e.printStackTrace();
                         }
-                        Log.d("Response", hexString.toString());
                     }
 
                     @Override
